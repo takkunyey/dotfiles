bec6633fcadd726f02b765f932a8fcb438dcee53
takkunyey <93082776+takkunyey@users.noreply.github.com>
2022-09-06 08:22:43 +0900

    elm_format

--- nvim/lua/pluginconfig/null-ls.lua
+++ nvim/lua/pluginconfig/null-ls.lua
@@ -2,6 +2,7 @@ require("null-ls").setup({
 	sources = {
 		require("null-ls").builtins.code_actions.eslint,
 		require("null-ls").builtins.diagnostics.eslint,
-    require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.formatting.elm_format,
 	},
 })
